43206e824a2a3071b1c3d6090dbfa98200beea2a
pyfuhr <susanooeditor@gmail.com>
2021-01-08 20:08:35 +0300

    Теперь двигается не герой, а камера

--- main.py
+++ main.py
@@ -25,11 +25,9 @@ class Map:
         for j in range(len(self.map)):
             for i in range(len(self.map[j])):
                 if self.map[j][i] == 1:
-                    screen.blit(self.wall, (i*32, j*32))
+                    screen.blit(self.wall, (640+i*32-MainHero.x, 310+j*32-MainHero.y))
                 if self.map[j][i] == 0:
-                    screen.blit(self.plintus, (i*32, j*32))
-                else:
-                    screen.blit(self.wall, (i*32, j*32))
+                    screen.blit(self.plintus, (640+i*32-MainHero.x, 310+j*32-MainHero.y))
 
     def check(self, x, y: int):
         if self.map[(y - 15) // 32][(x - 15) // 32] == 0 and self.map[(y - 15) // 32][(x + 15) // 32] == 0 and\
@@ -48,34 +46,31 @@ class Hero:
         self.flipped = False
 
     def draw(self, x=-1, y=-1):
-        global screen
         if x == -1 and y == -1:
-            screen.blit(self.image, (self.x - 16, self.y - 16))
-        else:
-            screen.blit(self.image, (x, y))
+            screen.blit(self.image, (640 - 16, 310 - 16))
 
     def punch(self, pov):
         R = 32
-        x1 = self.x + int(R * cos(pov * pi / 180))
-        y1 = self.y + int(R * sin(pov * pi / 180))
+        x1 = 640 + int(R * cos(pov * pi / 180))
+        y1 = 310 + int(R * sin(pov * pi / 180))
         x2 = x1
         y2 = y1
         for i in range(0, 100):
             rad = (i + pov) * pi / 180
-            pygame.draw.line(screen, (50+i, 50+i, 50+i), (self.x, self.y), (x2, y2), width=1)
+            pygame.draw.line(screen, (50+i, 50+i, 50+i), (640, 310), (x2, y2), width=1)
             pygame.draw.line(screen, 'white', (x1, y1), (x2, y2), width=1)
             x2 = x1
             y2 = y1
-            x1 = int(R * cos(rad)) + self.x
-            y1 = int(R * sin(rad)) + self.y
+            x1 = int(R * cos(rad)) + 640
+            y1 = int(R * sin(rad)) + 310
             self.draw()
             pygame.display.flip()
         time.sleep(0.5)
 
     def igni(self, pov):
         for R in range(20, 60):
-            x1 = self.x + int(R * cos(pov * pi / 180))
-            y1 = self.y + int(R * sin(pov * pi / 180))
+            x1 = 640 + int(R * cos(pov * pi / 180))
+            y1 = 310 + int(R * sin(pov * pi / 180))
             x2 = x1
             y2 = y1
             time.sleep(0.006)
@@ -85,14 +80,15 @@ class Hero:
                     pygame.draw.line(screen, (255, 255-(R-20)*6, 0), (x1, y1), (x2, y2), width=1)
                 x2 = x1
                 y2 = y1
-                x1 = round(R * cos(rad)) + self.x
-                y1 = round(R * sin(rad)) + self.y
+                x1 = round(R * cos(rad)) + 640
+                y1 = round(R * sin(rad)) + 310
             pygame.display.flip()
 
 
 if __name__ == '__main__':
     with open('maps/level1.map') as data:
         level1 = Map([list(map(int, i.split())) for i in data.readlines()])
+
     pygame.init()
     pygame.display.set_caption('PyG')
     run_image = pygame.image.load('data/main_run.png')
@@ -100,8 +96,10 @@ if __name__ == '__main__':
     font = pygame.font.Font(None, 30)
     MainLoop = True
     clock = pygame.time.Clock()
-    MainHero = Hero(300, 100)
+
+    MainHero = Hero(64, 64)
     MainHero.image.convert_alpha()
+
     while MainLoop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -110,6 +108,7 @@ if __name__ == '__main__':
                 KeyPressed.append(event.key)
             if event.type == pygame.KEYUP:
                 del KeyPressed[KeyPressed.index(event.key)]
+
         if KeyPressed:
             if pygame.K_ESCAPE in KeyPressed:
                 MainLoop = False
@@ -121,6 +120,7 @@ if __name__ == '__main__':
                 MainHero.pow = 1
                 if level1.check(MainHero.x, MainHero.y + 2):
                     MainHero.y += 2
+
             if pygame.K_a in KeyPressed:
                 MainHero.pow = 2
                 if level1.check(MainHero.x - 2, MainHero.y):
@@ -137,19 +137,32 @@ if __name__ == '__main__':
                     MainHero.image = pygame.transform.flip(MainHero.image, True, False)
                     run_image = pygame.transform.flip(run_image, True, False)
                     MainHero.flipped = False
+
             if pygame.K_SPACE in KeyPressed:
                 hud()
-                for i in range(15):
+                if MainHero.pow == 0:
+                    MainHero.x += 105
+                if MainHero.pow == 1:
+                    MainHero.y += 105
+                if MainHero.pow == 2:
+                    MainHero.x -= 105
+                if MainHero.pow == 3:
+                    MainHero.y -= 105
+                hud()
+                tempx = 640
+                tempy = 310
+                for i in range(1, 15):
                     if MainHero.pow == 0:
-                        MainHero.x += i
+                        tempx -= i
                     if MainHero.pow == 1:
-                        MainHero.y += i
+                        tempy -= i
                     if MainHero.pow == 2:
-                        MainHero.x -= i
+                        tempx += i
                     if MainHero.pow == 3:
-                        MainHero.y -= i
-                    screen.blit(run_image, (MainHero.x - 16, MainHero.y - 16))
-                    pygame.display.update()
+                        tempy += i
+                    screen.blit(run_image, (tempx - 16, tempy - 16))
+                    flip()
+                    MainHero.draw()
                     time.sleep(0.01)
                 MainHero.punch(MainHero.pow * 90 - 50)
             if pygame.K_q in KeyPressed:
