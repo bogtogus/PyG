98e8cb2ca11c3e080b145226b32b06812d801f62
feodor <pyfuhr@gmail.com>
2021-02-04 21:22:00 +0300

    Реализован Магазин + нормальное лобби

--- libs/items.py
+++ libs/items.py
@@ -6,12 +6,13 @@ class Weapon:
     def __init__(self, damage, accu):
         self.damage = damage
         self.accuracy = accu
+        self.cost = damage*accu
 
-    def DrawEquip(self, SCREEN:pygame.surface, size:list):
-        SCREEN.blit(Weapon.__image, (size[0] - 32, 32))
+    def DrawEquip(self, SCREEN:pygame.surface, size:list, image=__image):
+        SCREEN.blit(image, (size[0] - 32, 32))
 
-    def draw(self, SCREEN:pygame.surface, x, y: int):
-        SCREEN.blit(Weapon.__image, (x, y))
+    def draw(self, SCREEN:pygame.surface, x, y: int, image=__image):
+        SCREEN.blit(image, (x, y))
 
 
 class Armor:
@@ -20,9 +21,21 @@ class Armor:
     def __init__(self, defend, dod):
         self.defend = defend
         self.dodge = dod
+        self.cost = defend*dod
+
+    def DrawEquip(self, SCREEN: pygame.surface, size: list, image=__image):
+        SCREEN.blit(image, (size[0] - 64, 32))
+
+    def draw(self, SCREEN: pygame.surface, x, y: int, image=__image):
+        SCREEN.blit(image, (x, y))
+
+class WoodenStick(Weapon):
+    __image = pygame.image.load('data/wooden_stick.png')
+    def __init__(self):
+        super().__init__(4, 0.1)
 
     def DrawEquip(self, SCREEN: pygame.surface, size: list):
-        SCREEN.blit(Armor.__image, (size[0] - 64, 32))
+        super().DrawEquip(SCREEN, size, WoodenStick.__image)
 
     def draw(self, SCREEN: pygame.surface, x, y: int):
-        SCREEN.blit(Armor.__image, (x, y))
+        super().draw(SCREEN, x, y, WoodenStick.__image)
